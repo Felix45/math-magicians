5db9207ede750b0d66eacb139cacb3d076f1321b
Felix Ouma <fatonoh@gmail.com>
2022-06-28 23:18:41 +0300

    Add result container

---
+++ src/components/Result.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Result extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="bg-dark d-flex flex-end p-1">
+        0
+      </div>
+    );
+  }
+}
+
+export default Result;
