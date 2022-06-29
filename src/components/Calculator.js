4a66e90f6016c6c05b2aaa7fb3a1f3baceedb6db
Felix Ouma <fatonoh@gmail.com>
2022-06-29 20:02:25 +0300

    Add calculator logic to calculator components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,28 +2,42 @@ import React from 'react';
 import Button from './Button';
 import Result from './Result';
 import calculate from '../logic/calculator';
-import operate from '../logic/operate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: '',
+      operation: '',
+    };
+
+    this.handleCalc = this.handleCalc.bind(this);
+  }
+
+  handleCalc(event) {
+    this.setState((state) => calculate(state, event.target.textContent));
   }
 
   render() {
     const buttons = [
-      'AC', '+/-', '%', '/',
+      'AC', '+/-', '%', '÷',
       '7', '8', '9', 'x',
       '4', '5', '6', '-',
       '1', '2', '3', '+',
       '0', '.', '=',
     ];
 
+    const { total, next, operation } = this.state;
     return (
       <div className="d-flex flex-column calculator">
-        <Result />
+        <Result total={total} next={next} operation={operation} />
         <div className="d-flex calculator-btns">
-          {buttons.map((text) => <Button key={text.toString()} text={text} />)}
+          {
+            buttons.map((text) => (
+              <Button key={text.toString()} text={text} handleCalc={this.handleCalc} />
+            ))
+          }
         </div>
       </div>
     );
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Result extends React.Component {
   constructor(props) {
@@ -7,12 +8,26 @@ class Result extends React.Component {
   }
 
   render() {
+    const { total, next, operation } = this.props;
+    const result = `${total || operation || next ? `${total || ''} ${operation || ''} ${next || ''}` : 0}`;
     return (
       <div className="bg-dark d-flex flex-end p-1">
-        0
+        {result}
       </div>
     );
   }
 }
+
+Result.defaultProps = {
+  total: 0,
+  next: '',
+  operation: '',
+};
+
+Result.propTypes = {
+  total: PropTypes.number,
+  next: PropTypes.string,
+  operation: PropTypes.string,
+};
 
 export default Result;
