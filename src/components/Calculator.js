3065192fcdf47bb6da89c65cb32031a82fe9f7d3
Felix Ouma <fatonoh@gmail.com>
2022-06-30 15:01:02 +0300

    Refactor calculator component to functional component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import Result from './Result';
 import calculate from '../logic/calculator';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { total: '', next: '', operation: '' };
+const Calculator = () => {
+  const [calc, setCalc] = useState({ total: '', next: '', operation: '' });
+  const { total, next, operation } = calc;
 
-    this.handleCalc = this.handleCalc.bind(this);
-  }
+  const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
-  handleCalc(event) {
-    this.setState((state) => calculate(state, event.target.textContent));
-  }
+  const handleCalc = (event) => {
+    setCalc({ ...calc, ...calculate(calc, event.target.textContent) });
+  };
 
-  render() {
-    const buttons = [
-      'AC', '+/-', '%', '÷',
-      '7', '8', '9', 'x',
-      '4', '5', '6', '-',
-      '1', '2', '3', '+',
-      '0', '.', '=',
-    ];
-
-    const { total, next, operation } = this.state;
-    return (
-      <div className="d-flex flex-column calculator">
-        <Result total={total} next={next} operation={operation} />
-        <div className="d-flex calculator-btns">
-          {
-            buttons.map((text) => (
-              <Button key={text.toString()} text={text} handleCalc={this.handleCalc} />
-            ))
-          }
-        </div>
+  return (
+    <div className="d-flex flex-column calculator">
+      <Result total={total} next={next} operation={operation} />
+      <div className="d-flex calculator-btns">
+        {
+          buttons.map((text) => (
+            <Button key={text.toString()} text={text} handleCalc={handleCalc} />
+          ))
+        }
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
