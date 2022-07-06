8f3f5999f16e5853c135e3e9916368b5009552ba
Felix Ouma <fatonoh@gmail.com>
2022-07-06 15:22:17 +0300

    Fix livedemo homepage url

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,14 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   const urls = [
-    { id: 1, text: 'Home', url: '/math-magician' },
+    { id: 1, text: 'Home', url: '/math-magicians' },
     { id: 2, text: 'Calculator', url: '/calculator' },
     { id: 3, text: 'Quote', url: '/quote' },
   ];
   return (
     <nav className="d-flex space-between navbar">
       <header>
-        <h1 className="title">Math Magician</h1>
+        <h1 className="title">Math Magicians</h1>
       </header>
       <ul className="d-flex">
         {
